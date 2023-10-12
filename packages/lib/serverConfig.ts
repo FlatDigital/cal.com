bb488b14929c2f69e7addfd206aaf327bea5b548
Damian Crespi <damian.crespi@gmail.com>
2023-10-12 14:54:40 +0200

    chore: remove nodemailer function call

--- packages/lib/serverConfig.ts
+++ packages/lib/serverConfig.ts
@@ -1,4 +1,3 @@
-import nodemailer from "nodemailer";
 import postmarkTransport from "nodemailer-postmark-transport";
 import type SendmailTransport from "nodemailer/lib/sendmail-transport";
 import type SMTPConnection from "nodemailer/lib/smtp-connection";
@@ -7,13 +6,11 @@ import { isENVDev } from "@calcom/lib/env";
 
 function detectTransport(): SendmailTransport.Options | SMTPConnection.Options | string {
   if (process.env.EMAIL_POSTMARK_API_TOKEN) {
-    return nodemailer.createTransport(
-      postmarkTransport({
-        auth: {
-          apiKey: process.env.EMAIL_POSTMARK_API_TOKEN,
-        },
-      })
-    );
+    return postmarkTransport({
+      auth: {
+        apiKey: process.env.EMAIL_POSTMARK_API_TOKEN,
+      },
+    });
   }
   if (process.env.EMAIL_SERVER) {
     return process.env.EMAIL_SERVER;
